f2a0e2e85223b2eaa7cf62cad151222f2bd200a2
danielmeiborg <mail@danielmeiborg.com>
2023-05-13 16:20:43 +0200

    feat: landing page and installation guide

--- app.config.ts
+++ app.config.ts
@@ -1,24 +1,27 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    title: "Entromatica",
+    description: "A library for constructing, simulating and analyzing markov chains.",
+    url: "https://www.entromatica.com",
     socials: {
-      twitter: 'nuxtstudio',
-      github: 'nuxt-themes/docus'
+      github: "DanielMeiborg/entromatica"
     },
     aside: {
       level: 0,
       exclude: []
     },
     header: {
-      logo: true
+      logo: false
     },
     footer: {
       iconLinks: [
         {
-          href: 'https://nuxt.com',
-          icon: 'simple-icons:nuxtdotjs'
+          href: "https://crates.io/crates/entromatica",
+          icon: "vscode-icons:file-type-cargo"
+        },
+        {
+          href: "https://docs.rs/entromatica",
+          icon: "simple-icons:docsdotrs"
         }
       ]
     }
